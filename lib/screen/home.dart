da9d7e317e4a56be530f33fd249088ba47319c2b
นันทิน กุลณภา <khunmostz24@gmail.com>
2022-11-19 22:31:18 +0700

    test commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_tutorial/utils/constant.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kMyColor,
+        title: Text('Flutter Tutorial'),
+        leading: Icon(Icons.abc),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,4 @@
+
+import 'package:flutter/material.dart';
+
+const kMyColor = Color(0xFFF57C00);
